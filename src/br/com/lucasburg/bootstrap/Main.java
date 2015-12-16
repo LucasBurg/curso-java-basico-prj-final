dce765419594ceca2c6074764b37f6416440f849
Lucas Burg <lucasburgmota@gmail.com>
2015-12-16 17:00:47 -0200

    Bootstrap da aplicação

---
+++ src/br/com/lucasburg/bootstrap/Main.java
@@ -0,0 +1,12 @@
+package br.com.lucasburg.bootstrap;
+
+import br.com.lucasburg.form.UsuarioForm;
+import br.com.lucasburg.view.UsuarioView;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new UsuarioView(new UsuarioForm());
+	}
+
+}
