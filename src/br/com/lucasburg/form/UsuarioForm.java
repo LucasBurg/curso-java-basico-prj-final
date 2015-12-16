78a25c9db629ee53ddba80ff99ab185a8e6e145d
Lucas Burg <lucasburgmota@gmail.com>
2015-12-16 17:00:28 -0200

    Formulário para manutenção dos usuários

---
+++ src/br/com/lucasburg/form/UsuarioForm.java
@@ -0,0 +1,45 @@
+package br.com.lucasburg.form;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class UsuarioForm extends JPanel {
+	
+	private static final long serialVersionUID = 1667250075246796632L;
+	
+	
+	private final int HEIGHT = 25;
+	private final int WIDTH_TXT = 100;
+	
+	public UsuarioForm() {
+		super();
+		super.setLayout(null);
+		
+		JLabel lblId = new JLabel("Id");
+		lblId.setBounds(10, 10, 10, this.HEIGHT);
+		super.add(lblId);
+		
+		JTextField txtId = new JTextField();
+		txtId.setBounds(50, 10, this.WIDTH_TXT, this.HEIGHT);
+		txtId.setEnabled(false);
+		super.add(txtId);
+		
+		JLabel lblNome = new JLabel("Nome");
+		lblNome.setBounds(10, 40, 40, this.HEIGHT);
+		super.add(lblNome);
+		
+		JTextField txtNome = new JTextField();
+		txtNome.setBounds(50, 40, this.WIDTH_TXT, this.HEIGHT);
+		super.add(txtNome);
+		
+		JLabel lblEmail = new JLabel("E-mail");
+		lblEmail.setBounds(10, 70, 40, this.HEIGHT);
+		super.add(lblEmail);
+		
+		JTextField txtEmail = new JTextField();
+		txtEmail.setBounds(50, 70, this.WIDTH_TXT, this.HEIGHT);
+		super.add(txtEmail);
+		
+	}
+}
