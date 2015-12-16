641ef72c92a0e3b5af949129467680aeac228811
Lucas Burg <lucasburgmota@gmail.com>
2015-12-16 15:17:04 -0200

    Alteração de nome do arquivo

--- src/br/com/lucasburg/dao/AbstractManipulation.java
+++ src/br/com/lucasburg/dao/AbstractManipulation.java
@@ -1,6 +1,6 @@
 package br.com.lucasburg.dao;
 
-public abstract class AbstractDmlDao<T> {
+public abstract class AbstractManipulation<T> {
 	abstract void insert(T entity);
 	abstract void update(T entity);
 	abstract void delete(T entity);
